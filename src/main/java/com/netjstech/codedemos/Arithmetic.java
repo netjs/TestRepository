9c4673e4a00794b93fd936cac870980881930c03
netjs <60265414+netjs@users.noreply.github.com>
2024-08-02 14:09:54 +0530

    Update Arithmetic.java

--- src/main/java/com/netjstech/codedemos/Arithmetic.java
+++ src/main/java/com/netjstech/codedemos/Arithmetic.java
@@ -2,7 +2,7 @@ package com.netjstech.codedemos;
 
 public class Arithmetic {
    public int sum(int a, int b){
-	   if(a >= 10)
+	   if(a >= 12)
 		   throw new IllegalArgumentException("Value should be less than 10");
 	   return a + b;
    }
